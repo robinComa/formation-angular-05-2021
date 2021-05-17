45f2434226e629c671504861907145a78800df9a
Robin Coma Delperier <robin@placeme.io>
2021-05-17 15:14:51 +0200

    feat(Animal): remove service from root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { AnimalService } from './animal/shared/animal.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private animalService: AnimalService) {
-    console.log(this.animalService.get());
-  }
+  constructor() { }
 }
