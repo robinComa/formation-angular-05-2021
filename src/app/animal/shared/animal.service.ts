d54fe53c654a54defc165ba874088da023e78de5
Robin Coma Delperier <robin@placeme.io>
2021-05-19 15:46:01 +0200

    feat(Environment): api as config file

--- src/app/animal/shared/animal.service.ts
+++ src/app/animal/shared/animal.service.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 import { Animal } from './animal';
 
-const api = 'http://localhost:3000';
+const api = environment.api;
 
 @Injectable()
 export class AnimalService {
